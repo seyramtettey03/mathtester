363a2870b988cafde943b38999970352a25c1599
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-09-19 10:19:24 -0400

    explaining code

--- src/com/lickingheights/Main.java
+++ src/com/lickingheights/Main.java
@@ -16,8 +16,8 @@ public class Main {
         System.out.println(firstWord+ firstNumber+ secondnumber);
         System.out.println("firstNumber + seondnumber is"+ (firstNumber+secondnumber));
        System.out.println("firstNumber-secondmunber is"+(firstNumber-secondnumber));
-//        System.out.println("econdnumber /firstNumber is "+(secondnumber/firstNumber));
-//        System.out.println("firstNumber*econdmunber is"+(firstNumber*secondnumber));
+        System.out.println("econdnumber /firstNumber is "+(secondnumber/firstNumber));
+        System.out.println("firstNumber*econdmunber is"+(firstNumber*secondnumber));
 //        System.out.println("The moduluse of the secondNumber by the first number"+
 //                "is"+ (firstNumber+secondnumber));
 
