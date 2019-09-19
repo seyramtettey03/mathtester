a9dfb0f55ae8387cb368daf7c4a6c9bea1454e62
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-09-19 09:39:55 -0400

    initial commit

---
+++ src/com/lickingheights/Main.java
@@ -0,0 +1,63 @@
+package com.lickingheights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String firstWord;
+        String secondWord;
+        int firstNumber, secondnumber;
+
+        firstWord = "sun";
+        secondWord = "flower";
+        firstNumber = 6;
+        secondnumber = 12;
+
+        System.out.println (firstWord + secondWord);
+        System.out.println(firstWord+ firstNumber+ secondnumber);
+        System.out.println("firstNumber + seondnumber is"+ (firstNumber+secondnumber));
+        System.out.println("firstNumber-secondmunber is"+(firstNumber-secondnumber));
+        System.out.println("econdnumber /firstNumber is "+(secondnumber/firstNumber));
+        System.out.println("firstNumber*econdmunber is"+(firstNumber*secondnumber));
+        System.out.println("The moduluse of the secondNumber by the first number"+
+                "is"+ (firstNumber+secondnumber));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
