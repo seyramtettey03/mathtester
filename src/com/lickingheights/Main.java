74a98f89ac05637d74e99f4a761215b79124429f
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-09-19 09:43:12 -0400

    properly stage file - Walters

--- src/com/lickingheights/Main.java
+++ src/com/lickingheights/Main.java
@@ -7,19 +7,19 @@ public class Main {
         String secondWord;
         int firstNumber, secondnumber;
 
-        firstWord = "sun";
-        secondWord = "flower";
-        firstNumber = 6;
-        secondnumber = 12;
-
-        System.out.println (firstWord + secondWord);
-        System.out.println(firstWord+ firstNumber+ secondnumber);
-        System.out.println("firstNumber + seondnumber is"+ (firstNumber+secondnumber));
-        System.out.println("firstNumber-secondmunber is"+(firstNumber-secondnumber));
-        System.out.println("econdnumber /firstNumber is "+(secondnumber/firstNumber));
-        System.out.println("firstNumber*econdmunber is"+(firstNumber*secondnumber));
-        System.out.println("The moduluse of the secondNumber by the first number"+
-                "is"+ (firstNumber+secondnumber));
+//        firstWord = "sun";
+//        secondWord = "flower";
+//        firstNumber = 6;
+//        secondnumber = 12;
+//
+//        System.out.println (firstWord + secondWord);
+//        System.out.println(firstWord+ firstNumber+ secondnumber);
+//        System.out.println("firstNumber + seondnumber is"+ (firstNumber+secondnumber));
+//        System.out.println("firstNumber-secondmunber is"+(firstNumber-secondnumber));
+//        System.out.println("econdnumber /firstNumber is "+(secondnumber/firstNumber));
+//        System.out.println("firstNumber*econdmunber is"+(firstNumber*secondnumber));
+//        System.out.println("The moduluse of the secondNumber by the first number"+
+//                "is"+ (firstNumber+secondnumber));
 
 
 
